f8f7fa80a2c26916b6ecbf0223f58f8551fc7f42
Anthony Howell <anthony@howell-it.com>
2019-01-10 10:16:33 -0800

    first invoke api call commit, it works!

---
+++ src/Public/Invoke-FreshDeskAPICall.ps1
@@ -0,0 +1,29 @@
+<#
+API Docs: https://developers.freshdesk.com/api
+Github repo: https://github.com/ThePoShWolf/PSFreshDesk
+
+Just because I've done this before, doesn't mean I'm perfect or even everything
+according to best practice. So if you see something, say something.
+
+Apparently my office neighbors are noisy today, sorry about that.
+
+#>
+Function Invoke-FreshDeskAPICall {
+    Param(
+        [string]$uri,
+        [ValidateSet('Get','Post')]
+        [string]$method,
+        [string]$resource,
+        [string]$Domain = 'howellit',
+        [string]$apikey
+    )
+    $baseuri = "https://$Domain.freshdesk.com/api/v2"
+
+    $encodedapikey = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$apikey`:dummy"))
+
+    $headers = @{
+        Authorization = "Basic $encodedapikey"
+    }
+
+    Invoke-RestMethod -uri $baseuri/$resource -Method $method -Headers $headers
+}
